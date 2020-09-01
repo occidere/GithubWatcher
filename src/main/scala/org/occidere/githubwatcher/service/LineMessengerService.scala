a8eb3e64936455e73b9541e1589bdcb911b7d9d7
occidere <occidere@naver.com>
2020-09-01 19:39:33 +0900

    Complete repository message send

--- src/main/scala/org/occidere/githubwatcher/service/LineMessengerService.scala
+++ src/main/scala/org/occidere/githubwatcher/service/LineMessengerService.scala
@@ -5,6 +5,8 @@ import com.linecorp.bot.model.PushMessage
 import com.linecorp.bot.model.message.TextMessage
 import com.linecorp.bot.model.response.BotApiResponse
 import org.occidere.githubwatcher.logger.GithubWatcherLogger
+import org.occidere.githubwatcher.util.MessageBuilderUtils
+import org.occidere.githubwatcher.vo.{FollowerDiff, RepositoryDiff}
 
 /**
  * @author occidere
@@ -18,26 +20,12 @@ object LineMessengerService extends GithubWatcherLogger {
     .builder(s"${sys.env.getOrElse("gw_line_channel_token", "")}")
     .build()
 
-  def sendFollowerMessageIfExist(newFollowerLogins: List[String], deletedFollowerLogins: List[String]): Unit =
-    sendMessageIfExist(createFollowerMessage(newFollowerLogins, deletedFollowerLogins))
+  def sendFollowerMessage(diff: FollowerDiff): Unit = sendMessageIfExist(MessageBuilderUtils.createFollowerMessage(diff))
 
-  def sendRepositoryMessageIfExist(/* RepositoryDiff */): Unit = {} // TODO
+  def sendRepositoryMessage(repositoryDiff: RepositoryDiff): Unit = sendMessageIfExist(MessageBuilderUtils.createRepositoryMessage(repositoryDiff))
 
   private def sendMessageIfExist(msg: String): Unit = if (msg.nonEmpty) {
     val resp: BotApiResponse = lineMessagingClient.pushMessage(new PushMessage(BOT_ID, new TextMessage(msg))).get()
-    logger.info(s"Message send response: ${resp}")
+    logger.info(s"Message send response: $resp")
   }
-
-  private def createFollowerMessage(newFollowerLogins: List[String], deletedFollowerLogins: List[String]): String =
-    s"""${
-      if (newFollowerLogins.isEmpty) ""
-      else s"[${newFollowerLogins.size} of new Followers]\n${newFollowerLogins.sorted.mkString("\n")}\n"
-    }${
-      if (deletedFollowerLogins.isEmpty) ""
-      else s"[${deletedFollowerLogins.size} of deleted followers]\n${deletedFollowerLogins.sorted.mkString("\n")}\n"
-    }
-       |""".stripMargin
-
-  private def createRepositoryMessage(/* RepositoryDiff */): Unit = {} // TODO
-
 }
