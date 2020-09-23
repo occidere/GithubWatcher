674f71eeee4383bfba27f543f5e5bf906676ce2e
occidere <occidere@naver.com>
2020-09-23 15:04:03 +0900

    [BE-TASKS] Add API of Authenticated user's all reactions of issues

--- src/main/scala/org/occidere/githubwatcher/service/GithubApiService.scala
+++ src/main/scala/org/occidere/githubwatcher/service/GithubApiService.scala
@@ -2,7 +2,7 @@ package org.occidere.githubwatcher.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.occidere.githubwatcher.vo.{Repository, User}
+import org.occidere.githubwatcher.vo.{Reaction, Repository, User}
 import scalaj.http._
 
 /**
@@ -21,6 +21,9 @@ object GithubApiService {
     .map(_.getOrElse("login", "").toString)
     .filter(_.nonEmpty)
 
+  def getIssuesReactionsOfAuthUser: List[Reaction] = getBodies(s"issues", Map("per_page" -> "100", "filter" -> "created", "state" -> "all"))
+    .map(MAPPER.convertValue(_, classOf[Reaction]))
+
   def getRepositories(login: String): List[Repository] = getBodies(s"users/$login/repos").map(MAPPER.convertValue(_, classOf[Repository]))
 
   def getStargazerLogins(login: String, repo: String): List[String] = getLogins(login, repo)("stargazers")
@@ -33,23 +36,27 @@ object GithubApiService {
     .map(_.getOrElse("login", "").toString)
     .filter(_.nonEmpty)
 
-  private def getBody(url: String): Map[String, Any] = MAPPER.readValue(getResponse(url).body, classOf[Map[String, Any]])
+  private def getBody(url: String, params: Map[String, String] = Map()): Map[String, Any] = MAPPER.readValue(getResponse(url, params).body, classOf[Map[String, Any]])
 
-  private def getBodies(url: String): List[Map[String, Any]] = Iterator.from(1)
+  private def getBodies(url: String, params: Map[String, String] = Map()): List[Map[String, Any]] = Iterator.from(1)
     .map(page => MAPPER.readValue(
-      getResponse(s"$url?page=$page").body,
+      getResponse(url, params + ("page" -> page.toString)).body,
       classOf[List[Map[String, Any]]]))
     .takeWhile(_.nonEmpty)
     .flatten
     .toList
 
-  private def getResponse(url: String): HttpResponse[String] = Http(s"https://api.github.com/$url")
+  private def getResponse(url: String, params: Map[String, String] = Map()): HttpResponse[String] = Http(s"https://api.github.com/$url${toUrlEncoded(params)}")
     .option(HttpOptions.followRedirects(true))
     .headers(
       Seq(
         ("User-Agent", "Mozilla/5.0"),
-        ("Accept", "application/vnd.github.v3+json"),
+        ("Accept", "application/vnd.github.squirrel-girl-preview"),
         ("Authorization", if (TOKEN.nonEmpty) s"Bearer $TOKEN" else "")
       ).filter(_._2.nonEmpty)
     ).asString
+
+  private def toUrlEncoded(params: Map[String, String]): String =
+    if (params.nonEmpty) s"?${params.map(kv => s"${kv._1}=${kv._2}").mkString("&")}"
+    else ""
 }
