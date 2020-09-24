fb7c0d03c7ae96ee0fe7a8eeeab2bb56c642a925
occidere <occidere@naver.com>
2020-09-24 15:15:22 +0900

    [BE-TASKS] Complete ReactionDiff

--- src/main/scala/org/occidere/githubwatcher/vo/ReactionDiff.scala
+++ src/main/scala/org/occidere/githubwatcher/vo/ReactionDiff.scala
@@ -7,6 +7,15 @@ package org.occidere.githubwatcher.vo
  * @since 2020-09-23
  */
 case class ReactionDiff(prevReaction: Reaction, latestReaction: Reaction) {
-  val repoName: String = latestReaction.repoName
+  val thumbUpDelta: Int = prevReaction.thumbUp - latestReaction.thumbUp
+  val thumbDownDelta: Int = prevReaction.thumbDown - latestReaction.thumbDown
+  val laughDelta: Int = prevReaction.laugh - latestReaction.laugh
+  val hoorayDelta: Int = prevReaction.hooray - latestReaction.hooray
+  val heartDelta: Int = prevReaction.heart - latestReaction.heart
+  val rocketDelta: Int = prevReaction.rocket - latestReaction.rocket
+  val eyesDelta: Int = prevReaction.eyes - latestReaction.eyes
+  val confusedDelta: Int = prevReaction.confused - latestReaction.confused
 
+  val hasChanged: Boolean = thumbUpDelta != 0 || thumbDownDelta != 0 || laughDelta != 0 ||
+    hoorayDelta != 0 || heartDelta != 0 || rocketDelta != 0 || eyesDelta != 0 || confusedDelta != 0
 }
