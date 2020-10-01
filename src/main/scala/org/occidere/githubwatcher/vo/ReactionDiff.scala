4c22b9e2f3251215d1d45c3c9ed2d5a433ce793e
occidere <occidere@naver.com>
2020-10-01 15:44:49 +0900

    [BE-ReactionWatchTask] Swap prev & latest pos to calcuating delta

--- src/main/scala/org/occidere/githubwatcher/vo/ReactionDiff.scala
+++ src/main/scala/org/occidere/githubwatcher/vo/ReactionDiff.scala
@@ -7,14 +7,14 @@ package org.occidere.githubwatcher.vo
  * @since 2020-09-23
  */
 case class ReactionDiff(prevReaction: Reaction, latestReaction: Reaction) {
-  val thumbUpDelta: Int = prevReaction.thumbUp - latestReaction.thumbUp
-  val thumbDownDelta: Int = prevReaction.thumbDown - latestReaction.thumbDown
-  val laughDelta: Int = prevReaction.laugh - latestReaction.laugh
-  val hoorayDelta: Int = prevReaction.hooray - latestReaction.hooray
-  val heartDelta: Int = prevReaction.heart - latestReaction.heart
-  val rocketDelta: Int = prevReaction.rocket - latestReaction.rocket
-  val eyesDelta: Int = prevReaction.eyes - latestReaction.eyes
-  val confusedDelta: Int = prevReaction.confused - latestReaction.confused
+  val thumbUpDelta: Int = latestReaction.thumbUp - prevReaction.thumbUp
+  val thumbDownDelta: Int = latestReaction.thumbDown - prevReaction.thumbDown
+  val laughDelta: Int = latestReaction.laugh - prevReaction.laugh
+  val hoorayDelta: Int = latestReaction.hooray - prevReaction.hooray
+  val heartDelta: Int = latestReaction.heart - prevReaction.heart
+  val rocketDelta: Int = latestReaction.rocket - prevReaction.rocket
+  val eyesDelta: Int = latestReaction.eyes - prevReaction.eyes
+  val confusedDelta: Int = latestReaction.confused - prevReaction.confused
 
   val hasChanged: Boolean = thumbUpDelta != 0 || thumbDownDelta != 0 || laughDelta != 0 ||
     hoorayDelta != 0 || heartDelta != 0 || rocketDelta != 0 || eyesDelta != 0 || confusedDelta != 0
